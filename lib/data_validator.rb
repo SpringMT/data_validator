2a1dc456c0e4b13886853a72361d74b5102d4386
Spring_MT <today.is.sky.blue.sky@gmail.com>
2013-03-11 15:51:29 +0900

    Inherit is false in const_get DataValidator
    * http://ref.xaio.jp/ruby/classes/module/const_get

--- lib/data_validator.rb
+++ lib/data_validator.rb
@@ -22,10 +22,9 @@ module DataValidator
         next if @params[key].blank? && is_allow_blank
 
         rule.each_pair do |validator, options|
-          klass = "#{validator.to_s.camelize}Validator"
+          klass = "DataValidator::#{validator.to_s.camelize}Validator"
           constant   = Object
-          constant   = constant.const_get "DataValidator"
-          validation = constant.const_get(klass).new(key, @params[key], options, errors)
+          validation = constant.const_get(klass, false).new(key, @params[key], options, errors)
           validation.check_validity!
           validation.validate
         end
